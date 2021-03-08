2367656fb19f3cf265c0566166ed9240463aabd7
korneix <arno.marty@etu.u-bordeaux.fr>
2021-03-08 16:39:19 +0100

    Besoin 1 implémenté

--- backend/src/main/java/pdl/backend/ImageDao.java
+++ backend/src/main/java/pdl/backend/ImageDao.java
@@ -1,12 +1,11 @@
 package pdl.backend;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
@@ -19,16 +18,39 @@ public class ImageDao implements Dao<Image> {
   private final Map<Long, Image> images = new HashMap<>();
 
   public ImageDao() {
-    // placez une image test.jpg dans le dossier "src/main/resources" du projet
-    final ClassPathResource imgFile = new ClassPathResource("test.jpg");
+    ClassPathResource imgFile;
     byte[] fileContent;
+
     try {
-      fileContent = Files.readAllBytes(imgFile.getFile().toPath());
-      Image img = new Image("logo.jpg", fileContent);
-      images.put(img.getId(), img);
+      for(String filename : resourcesList("src/main/resources", null)) {
+        imgFile = new ClassPathResource(filename);
+        fileContent = Files.readAllBytes(imgFile.getFile().toPath());
+        Image img = new Image(filename, fileContent);
+        images.put(img.getId(), img);
+      }
     } catch (final IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public Set<String> resourcesList(String dir, String subdir) throws IOException {
+    File[] f = new File(dir).listFiles();
+    Set<String> res = new HashSet<>();
+    if(f == null) return res;
+
+    for( File entry : f){
+      if(entry.isDirectory()){
+        res.addAll(resourcesList(entry.getPath(), entry.getName() + "/"));
+      }
+
+      String mimeType = Files.probeContentType(entry.toPath());
+      if(mimeType != null && (mimeType.equals("image/jpeg") || mimeType.equals("image/tiff")) ){
+        if(subdir != null)
+          res.add(subdir + entry.getName());
+        else res.add(entry.getName());
+      }
+    }
+    return res;
   }
 
   @Override
